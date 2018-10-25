ef913011f393752fce5b8cfd0fc1eee492597618
Andrew <krylar808@gmail.com>
2018-10-25 01:29:37 +0000

    1.3.5 - troop.js: formatting fixes

--- commands/troop.js
+++ commands/troop.js
@@ -10,17 +10,17 @@ exports.run = async (client, message, args, level) => {
 
   // Load TCR
   ndx = client.tcrTroops.worksheets.findIndex(n => n.title === "REF_Troops");
-  client.tcrTroops.getRows(ndx+1, {offset: 3}, function (err, rows) {
+  client.tcrTroops.getRows(ndx+1, {offset: 40}, function (err, rows) {
 //    console.log(`==> Keys: ${Object.keys(rows[0])}`);
 
     rows.forEach(rr => {
-      console.log();
+//      console.log();
 
       // abbreviate troop types
       var type1 = rr.type.replace('Infantry','INF').replace('Walker','WLK').replace('Airship','AIR');
       var type2 = "";
-      if(rr._cre1l)
-        type2 = " / " + rr.ndtype.replace('Infantry','INF').replace('Walker','WLK').replace('Airship','AIR');
+      if(rr.type2)
+        type2 = " / " + rr.type2.replace('Infantry','INF').replace('Walker','WLK').replace('Airship','AIR');
 
       // build output
       msg = `
@@ -28,22 +28,23 @@ Troop    : ${rr.troop||""}
 Type     : ${type1}${type2}
 Tier     : ${rr.tier||""}
 Units    : ${rr.units||""}
-Capacity : ${rr.capacity||""}
-HP       : ${rr.hp||""}
-ATK      : ${rr.atk||""}
-DEF      : ${rr.def||""}
-CRIT     : ${rr.crit||""}
-ACC      : ${rr.acc||""}
-DOD      : ${rr.dod||""}
+Capacity : ${rr.capacity.trim()||""}
+HP       : ${rr.hp.trim()||""}
+ATK      : ${rr.atk.trim()||""}
+DEF      : ${rr.def.trim()||""}
+CRIT     : ${rr.crit.trim()||""}
+ACC      : ${rr.acc.trim()||""}
+DOD      : ${rr.dod.trim()||""}
 Food     : ${rr.food||""}
-Parts    : ${rr.parts||""}
-Electric : ${rr.ele||""}
-Gas      : ${rr.gas||""}
-Cash     : ${rr.cash||""}
-SM       : ${rr.sm||""}
-Rep      : ${rr.rep||""}
-UC       : ${rr.uc||""}
-Power    : ${rr.power||""}
+Parts    : ${rr.parts||""}`
+      if(rr.ele) msg += `\nElectric : ${rr.ele||""}`
+      if(rr.gas) msg += `\nGas      : ${rr.gas||""}`
+      if(rr.cash) msg += `\nCash     : ${rr.cash||""}`
+      if(rr.sm) msg += `\nSM       : ${rr.sm||""}`
+      if(rr.rep) msg += `\nRep      : ${rr.rep||""}`
+      if(rr.uc) msg += `\nUC       : ${rr.uc||""}`
+      if(rr.hc) msg += `\nHC       : ${rr.hc||""}`
+      msg += `\nPower    : ${rr.power||""}
 KE       : ${rr.ke||""}`;
 
 //      console.log(message);
